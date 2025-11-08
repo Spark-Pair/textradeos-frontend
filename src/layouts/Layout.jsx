91301a435d12b0ccc3e72fb94408e701975bc174
decorgraphicsstudio <hasanraza03165825495@gmail.com>
2025-11-08 11:34:29 +0500

    .

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Button from "../components/Button";
+import Notifications from "../components/Notifications";
 
 export default function Layout({ children }) {
   return (
     <div className="h-screen flex flex-col bg-[#eef5f5] overflow-hidden relative">
       {/* 🔹 Main Content */}
       <div className="p-5 h-full overflow-y-auto">{children || <Outlet />}</div>
+
+      <Notifications />
 
       {/* 🔹 Floating Bottom Bar */}
       <div className="fixed bottom-3 left-1/2 -translate-x-1/2 flex items-center justify-between space-x-1 bg-[#f8fbfb] shadow-md border border-gray-300 p-1 rounded-2xl">
